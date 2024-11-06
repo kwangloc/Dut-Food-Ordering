a0145c57f0ff9432250bca9c1ad31a3e43dd715e
kwangloc <tqloc1409@gmail.com>
2024-11-06 18:25:34 +0700

    first commit

---
+++ src/middleware/auth.ts
@@ -0,0 +1,58 @@
+import { auth } from "express-oauth2-jwt-bearer";
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import User from "../models/user";
+
+// To add customer properties to request
+declare global {
+  namespace Express {
+    interface Request {
+      userId: string;
+      auth0Id: string;
+    }
+  }
+}
+
+export const jwtCheck = auth({
+  audience: process.env.AUTH0_AUDIENCE,
+  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
+  tokenSigningAlg: "RS256",
+});
+
+export const jwtParse = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { authorization } = req.headers;
+
+  console.log(authorization);
+
+  if (!authorization || !authorization.startsWith("Bearer ")) {
+    return res.sendStatus(401);
+  }
+
+  console.log("HERE");
+  const token = authorization.split(" ")[1];
+
+  try {
+    const decoded = jwt.decode(token) as jwt.JwtPayload;
+    console.log("decoded:", decoded);
+    console.log("decoded.sub", decoded.sub);
+
+    // const auth0Id = decoded.sub;
+    const auth0Id = decoded.auth0Id;
+
+    const user = await User.findOne({ auth0Id });
+
+    if (!user) {
+      return res.sendStatus(401);
+    }
+
+    req.auth0Id = auth0Id as string;
+    req.userId = user._id.toString();
+    next();
+  } catch (error) {
+    return res.sendStatus(401);
+  }
+};
---
+++ src/routes/MyUserRoute.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import MyUserController from "../controllers/MyUserController";
+//
+import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyUserRequest } from "../middleware/validation";
+
+const router = express.Router();
+
+// /api/my/user
+// router.get("/", jwtCheck, jwtParse, MyUserController.getCurrentUser);
+router.get("/", MyUserController.getCurrentUser);
+router.post("/", MyUserController.createCurrentUser);
+// router.post("/", jwtCheck, MyUserController.createCurrentUser);
+// router.post("/", MyUserController.createCurrentUser);
+// router.put("/", jwtCheck, jwtParse, validateMyUserRequest, MyUserController.updateCurrentUser);
+router.put("/", jwtParse, validateMyUserRequest, MyUserController.updateCurrentUser);
+
+export default router;
